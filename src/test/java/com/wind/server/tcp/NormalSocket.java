7c4c6fa9c49682c7bc64d2685d41633afc7ed096
windspear07 <windspear@foxmail.com>
2018-01-03 09:00:39 +0800

    add basic socket

---
+++ src/test/java/com/wind/server/tcp/NormalSocket.java
@@ -0,0 +1,46 @@
+package com.wind.server.tcp;
+
+import lombok.extern.slf4j.Slf4j;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * just a demo of basic socket server
+ */
+@Slf4j
+public class NormalSocket {
+
+    public static void main(String[] agrs) throws IOException {
+        new NormalSocket().runServer();
+    }
+
+
+    public static int PORT = 8081;
+
+    public void runServer() throws IOException {
+        ServerSocket s = new ServerSocket(PORT);
+
+        while (true) {
+            Socket socket = s.accept();
+            log.info("New connection: {}", socket);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            String str;
+            while ( (str = br.readLine()) != null) {
+
+                if (str.equals("END")) {
+                    break;
+                }
+                System.out.println("receive:" + str);
+                pw.println("ok");
+                pw.flush();
+            }
+
+            br.close();
+            pw.close();
+            socket.close();
+        }
+    }
+}
